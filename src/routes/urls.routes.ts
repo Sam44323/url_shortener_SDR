37c5b37e2bd38c8785909e8780ea6d9ba2fe24a1
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-05 02:09:19 +0530

    add: added docs for routers

--- src/routes/urls.routes.ts
+++ src/routes/urls.routes.ts
@@ -6,11 +6,11 @@ import {
   deleteTinyUrl
 } from '../controllers/urls.controllers'
 
-const urlRouter = Router()
+const urlRouter = Router() // router initialization for url
 
-urlRouter.get('/test', test)
-urlRouter.post('/generate', generate)
-urlRouter.get('/tinyurl', getTinyUrl)
-urlRouter.delete('/tinyurl/:url_id', deleteTinyUrl)
+urlRouter.get('/test', test) // testing the url controller
+urlRouter.post('/generate', generate) // generating a new short_url
+urlRouter.get('/tinyurl', getTinyUrl) // getting the short_url
+urlRouter.delete('/tinyurl/:url_id', deleteTinyUrl) // deleting the short_url
 
 export default urlRouter
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,13 +8,13 @@ import {
   deleteUser
 } from '../controllers/users.controllers'
 
-const userRouter = Router()
+const userRouter = Router() // router initialization for url
 
-userRouter.get('/test', test)
-userRouter.post('/addUser', addUser)
-userRouter.get('/userDetails', getUserDetails)
-userRouter.patch('/updateUser', updateUser)
-userRouter.patch('/refreshToken', refreshToken)
-userRouter.delete('/deleteUser', deleteUser)
+userRouter.get('/test', test) // testing the url controller
+userRouter.post('/addUser', addUser) // adding a new user
+userRouter.get('/userDetails', getUserDetails) // getting the user details
+userRouter.patch('/updateUser', updateUser) // updating the user details
+userRouter.patch('/refreshToken', refreshToken) // refreshing the api_token
+userRouter.delete('/deleteUser', deleteUser) // deleting the user
 
 export default userRouter
